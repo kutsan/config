634e0a2805658386cefc7b83777be0576d114494
Kutsan Kaplan <me@kutsan.dev>
2021-01-15 04:37:47 +0300

    prettier: use 2-width spaces

--- prettier/index.js
+++ prettier/index.js
@@ -1,18 +1,6 @@
 module.exports = {
-	printWidth: 80,
-	tabWidth: 4,
-	useTabs: true,
-	semi: false,
-	singleQuote: true,
-	quoteProps: 'as-needed',
-	jsxSingleQuote: true,
-	trailingComma: 'none',
-	bracketSpacing: true,
-	jsxBracketSameLine: false,
-	arrowParens: 'always',
-	requirePragma: false,
-	insertPragma: false,
-	proseWrap: 'preserve',
-	htmlWhitespaceSensitivity: 'css',
-	endOfLine: 'auto'
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none'
 }
